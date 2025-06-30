34460637be7d8d5b853b084a93209da88075de32
warudo <muhammadsalwarud@gmail.com>
2025-06-30 16:42:48 +0700

    feat: for encrypt the password for better security

---
+++ src/java/com/karyawan/model/Enkripsi.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.karyawan.model;
+
+/**
+ *
+ * @author Warudo
+ */
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+public class Enkripsi {
+    public static String sha256(String data) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(data.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
